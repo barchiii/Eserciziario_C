63f5b05d329f08fed2c6d81135666cf93ccc33cb
barchiii <filippo.barchi@iisviolamarchesini.edu.it>
2025-03-26 08:37:34 +0100

    esercizio 02_wc

---
+++ 4E/programmazione di sistema/02_wc.c
@@ -0,0 +1,69 @@
+/* Scrivere un programma concorrente che realizzi il seguente comando:
+
+cat file.txt | wc 
+
+il processo p2 deve rimandare l'output di wc al padre, il quale lo scriverà
+su un file chiamato wc.txt
+
+il nome del file "file.txt" deve essere passato come argomento in argv[1] */
+
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <sys/wait.h>
+
+int main(int argc, char *argv[])
+{
+    if (argc != 2)
+    {
+        printf("Numero argomenti sbagliato\n");
+        exit(1);
+    }
+
+    int p1p0[2], pid, p2p0[2];
+    pipe(p1p0);
+
+    pid = fork();
+
+    if (pid == 0)
+    {
+        close(p1p0[0]);
+        close(1);
+        dup(p1p0[1]);
+        close(p1p0[1]);
+        execl("/bin/cat", "cat", argv[1], (char *)0);
+        return -1;
+    }
+
+    pipe(p2p0);
+    pid = fork();
+
+    if (pid == 0)
+    {
+        close(p1p0[1]);
+        close(0);
+        dup(p1p0[0]);
+        close(p1p0[0]);
+        
+        close(p2p0[0]);
+        close(1);
+        dup(p2p0[1]);
+        close(p2p0[1]);
+        execl("/usr/bin/wc", "wc", (char *)0);
+        return -1;
+
+    }
+
+    close(p1p0[1]);
+    close(p1p0[0]);
+    close(p2p0[1]);
+
+    close(0);
+    dup(p2p0[0]);
+    close(p2p0[0]);
+
+    close(1);
+    open(wc.txt, O_WRONLY | O_CREAT, 0777);
+
+    execl("/usr/bin/tee", "tee", (char *)0);
+}
